e7aa0167c92cb6bf0a2438ab26314dd882888412
juns1984@qq.com <juns1984@qq.com>
2015-04-12 11:49:00 +0800

    v

---
+++ simactor/kvs.erl
@@ -0,0 +1,31 @@
+-module(kvs).
+-export([start/0,store/2,lookup/1]).
+
+start() ->
+     register(kvs,spawn(fun() -> loop() end)).
+
+store(K,V) ->
+    rpc({store,K,V}).
+
+lookup(K) ->
+    rpc({lookup,K}).
+
+rpc(Q) ->
+    kvs ! {self(),Q},
+    receive 
+        {kvs,Reply} ->
+            Reply
+    end.
+
+loop()->
+    receive
+       {From,{store,K,V}} ->
+            put (K,{ok,V}),
+            From ! {kvs,true},
+            loop();
+        {From,{lookup,K}} ->
+            From ! {kvs,get(K)},
+            loop()
+    end.
+
+            
